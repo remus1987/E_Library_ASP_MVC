db491729868647fd3938b74d9a8000a3d16ad0b4
Remus <remusiftimie87@gmail.com>
2020-01-12 21:53:11 +0000

    Added Contact and About Views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,17 @@ namespace E_Library.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
